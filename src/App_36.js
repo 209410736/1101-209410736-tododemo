20657a1a9edc9056873fdc0d5e2a32ca686bd16d
209410736 <209410736@gms.tku.edu.tw>
2021-09-30 19:54:53 +0800

    first commit

---
+++ src/App_36.js
@@ -0,0 +1,15 @@
+import Todo_36 from './components/Todo_36';
+import './App.css';
+
+function App_36() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+  <Todo_36 text ='Learb React'/>
+  <Todo_36 text ='Learn Test'/>
+  <Todo_36 text ='Learb Hello'/>
+    </div>
+  );
+}
+
+export default App_36;
