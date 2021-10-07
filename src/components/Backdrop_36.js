a6f0ea207aabff348651f5e1be4d7aa41e32d877
209410736 <209410736@gms.tku.edu.tw>
2021-10-07 20:11:54 +0800

    123

---
+++ src/components/Backdrop_36.js
@@ -0,0 +1,7 @@
+function Backdrop_36(props) {
+    return (
+        <div className="backdrop" onClick={props.onClose}></div>
+    )
+}
+
+export default Backdrop_36;
--- src/components/Todo_36.js
+++ src/components/Todo_36.js
@@ -1,16 +1,32 @@
+import { useState } from 'react';
+import Modal_36 from '../components/Modal_36';
 import React from 'react'
+import Backdrop_36 from '../components/Backdrop_36';
 
 function Todo_36(props) {
-    return (
-        <div>
-            <div className="card">
-    <h2>{props.text}</h2>
-    <div className="actions">
-      <button className="btn">Delete</button>
-    </div>
-  </div>
+  const [showModal, setShowModal] = useState();
+
+  function showModalHandler() {
+    setShowModal(true);
+
+  }
+
+  function closeModalHandler() {
+    setShowModal(false);
+  }
+
+  return (
+    <div>
+      <div className="card">
+        <h2>{props.text}</h2>
+        <div className="actions">
+          <button className="btn" onClick={showModalHandler}>Delete</button>
         </div>
-    )
+      </div>
+      {showModal && <Backdrop_36 onClose={closeModalHandler} />}
+      {showModal && <Modal_36 text='Are you sure?' onClose={closeModalHandler} />}
+    </div>
+  );
 }
 
-export default Todo_36
+export default Todo_36;
